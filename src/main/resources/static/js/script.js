ad448628cc3c21bb5d886c819d0366cbf0db7901
larsbenedetto <lbenedetto@toogoodtogo.com>
2021-04-13 18:24:26 -0700

    Stub out more frontend code

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,25 +1,59 @@
 function hostGame() {
+    let playerName = $("#InputUsernameField").text();
     post("/api/lobby/v1/",
         {
-            "hostName": $("#InputUsernameField").text(),
+            "hostName": playerName,
             "isParticipating": true
         },
         result => {
-            console.log(result)
+            let playerId = result.data.ownerId
+            let lobbyCode = result.data.id
+            // TODO: store these in the session
         }
     )
 }
 
 function joinGame() {
-    console.log("join game clicked")
+    let playerName = $("#InputUsernameField").text();
+    let lobbyId = "asdf" // TODO: Get this from the UI
+    post("/api/lobby/v1/" + lobbyId + "/join",
+        { "playerName": playerName },
+        result => {
+            let playerId = result.data.id
+            // TODO: store this in the session
+        }
+    )
 }
 
 function startGame() {
-
+    let lobbyId = "asdf" // TODO: Get this from the session
+    let playerId = "asdf" // TODO: Get this from the session
+    post("/api/lobby/v1/" + lobbyId + "/start",
+        { "playerId": playerId },
+        result => {
+            // TODO: Navigate the user to the view question page
+            // Players who aren't hosting will hopefully receive a stomp notification
+        }
+    )
 }
 
 function submitEstimate() {
-
+    let lobbyId = "asdf" // TODO: Get this from the session
+    let playerId = "asdf" // TODO: Get this from the session
+    let lowerBound = "asdf" // TODO: Get this from the UI
+    let upperBound = "asdf" // TODO: Get this from the UI
+    post("/api/lobby/v1/" + lobbyId + "/submitEstimate",
+        {
+            "playerId": playerId,
+            "lowerBound": lowerBound,
+            "upperBound": upperBound
+        },
+        result => {
+            let score = result.data.score
+            // TODO: Display the users score in the UI
+            // When other players have answered / times up, move everyone to the results page
+        }
+    )
 }
 
 function connect() {
@@ -35,6 +69,7 @@ function connect() {
 }
 
 function post(url, data, onSuccess) {
+    console.log("POST " + url)
     $.ajax({
         type: "POST",
         url: url,
