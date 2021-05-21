6f6b407dcac718ba09c77030f008fd4ac086e355
larsbenedetto <lbenedetto@toogoodtogo.com>
2021-05-21 08:36:34 -0700

    Fix some scoring issues

--- src/main/kotlin/com/larsbenedetto/confidentlyincorrect/usecase/service/ScoringService.kt
+++ src/main/kotlin/com/larsbenedetto/confidentlyincorrect/usecase/service/ScoringService.kt
@@ -22,18 +22,33 @@ class ScoringService {
     ): Double {
         val adjustedLowerBound = scoringType.adjustLowerBound(lowerBound)
         val adjustedUpperBound = scoringType.adjustUpperBound(upperBound)
+        val adjustedScore =calculateScoreWithAdjustedBounds(scoringType, correctAnswer, adjustedLowerBound, adjustedUpperBound);
+        val score = calculateScoreWithAdjustedBounds(scoringType, correctAnswer, lowerBound, upperBound)
+        return max(score, adjustedScore)
+    }
+
+    private fun calculateScoreWithAdjustedBounds(
+        scoringType: ScoringType,
+        correctAnswer: Double,
+        lowerBound: Double,
+        upperBound: Double
+    ): Double {
+        if(lowerBound == upperBound && lowerBound == correctAnswer) {
+            return maxScore
+        }
+
         // How far between the lower bound and the correct answer
-        val lowerBoundDistance = scoringType.calculateLowerBoundDistance(adjustedLowerBound, correctAnswer) / scoringType.scale
+        val lowerBoundDistance = scoringType.calculateLowerBoundDistance(lowerBound, correctAnswer) / scoringType.scale
         // How far between the upper bound and the correct answer
-        val upperBoundDistance = scoringType.calculateUpperBoundDistance(correctAnswer, adjustedUpperBound) / scoringType.scale
+        val upperBoundDistance = scoringType.calculateUpperBoundDistance(correctAnswer, upperBound) / scoringType.scale
         // How far between the lower and upper bound
-        val intervalWidth = scoringType.calculateIntervalWidth(adjustedLowerBound, adjustedUpperBound) / scoringType.scale
+        val intervalWidth = scoringType.calculateIntervalWidth(lowerBound, upperBound) / scoringType.scale
 
         val score = when {
-            correctAnswer < adjustedLowerBound -> {
+            correctAnswer < lowerBound -> {
                 ((-2 / (1 - confidence)) * lowerBoundDistance) - ((lowerBoundDistance / (1 + lowerBoundDistance)) * intervalWidth)
             }
-            correctAnswer > adjustedUpperBound -> {
+            correctAnswer > upperBound -> {
                 ((-2 / (1 - confidence)) * upperBoundDistance) - ((upperBoundDistance / (1 + upperBoundDistance)) * intervalWidth)
             }
             else -> {
